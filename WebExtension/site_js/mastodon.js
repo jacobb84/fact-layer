dc25fe6d9688f947f1d53559b15c36188af76534
invalid character <invalidcharacter0x86@gmail.com>
2019-08-07 22:24:32 -0400

    Added counter.social support

---
+++ WebExtension/site_js/mastodon.js
@@ -0,0 +1,60 @@
+
+// configuration of the observer:
+const config = { childList: true, subtree: true, attributes: true, attributeFilter: ['class'] };
+
+function decorateNode(node) {
+    var $node = $(node),
+        $links = $node.find('a.status-link, a.status-card')
+    $links.each(function(ix, link){
+        var $link = $(link),
+            domain = $link.attr('href');
+        browser.runtime.sendMessage({command: "getWebsite", domain: domain}, function(response) {
+            $link.addClass(getCSS(response.overallBias, response.websiteResult.OrganizationType));
+            $link.attr("title", response.websiteResult.Name + " | " + response.biasText);
+        });
+    });
+}
+function watchColumn(column) {
+    var target = column;
+        // create an observer instance
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                mutation.addedNodes.forEach(function(node) {
+                    if (node.tagName == 'ARTICLE'){
+                        decorateNode(node);
+                    }
+                });
+                $(mutation.target).find('.status__wrapper, .detailed-status__wrapper').each(function(ix, node){
+                    decorateNode(node);
+                });
+            });    
+        });
+
+        observer.observe(target, config);
+}
+
+//Handle chrome
+if (typeof browser === 'undefined') {
+    browser = chrome;
+}
+
+if (browser) {
+
+
+	$("div.column").each(function (index, obj) {
+		watchColumn(obj);
+    });
+
+    // and when new columns are created
+    var colObserver = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation){
+            mutation.addedNodes.forEach(function(node) {
+                if (node.tagName == 'DIV' && node.classList.contains('column')){
+                    watchColumn(node);
+                }
+            });
+        });
+    });
+
+    colObserver.observe(document, config);
+}
